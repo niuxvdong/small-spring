bb9dd1ef8cccb531abe308efbd08890da973d28a
niuxvdong <158903258@qq.com>
2023-04-14 23:38:15 +0800

    feat：第五章-实现资源加载器加载xml文件注册Bean

--- small-spring-05/src/test/java/cn/itnxd/springframework/ApiTest.java
+++ small-spring-05/src/test/java/cn/itnxd/springframework/ApiTest.java
@@ -1,5 +1,6 @@
 package cn.itnxd.springframework;
 
+import cn.hutool.core.io.IoUtil;
 import cn.itnxd.springframework.bean.UserMapper;
 import cn.itnxd.springframework.bean.UserService;
 import cn.itnxd.springframework.beans.PropertyValue;
@@ -8,7 +9,12 @@ import cn.itnxd.springframework.beans.factory.BeanFactory;
 import cn.itnxd.springframework.beans.factory.config.BeanDefinition;
 import cn.itnxd.springframework.beans.factory.config.BeanReference;
 import cn.itnxd.springframework.beans.factory.support.DefaultListableBeanFactory;
+import cn.itnxd.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import cn.itnxd.springframework.core.io.DefaultResourceLoader;
+import cn.itnxd.springframework.core.io.Resource;
 import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  * @Author niuxudong
@@ -19,21 +25,34 @@ import org.junit.Test;
 public class ApiTest {
 
     @Test
+    public void test_resource() throws IOException {
+        // classpath 文件
+        DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource = resourceLoader.getResource("classpath:hello.txt");
+        String res = IoUtil.readUtf8(resource.getInputStream());
+        System.out.println(res);
+
+        // url 流
+        Resource urlResource = resourceLoader.getResource("https://www.baidu.com");
+        String urlRes = IoUtil.readUtf8(urlResource.getInputStream());
+        System.out.println(urlRes);
+
+        // 本地文件流
+        Resource fileResource = resourceLoader.getResource("src/main/resources/hello.txt");
+        String fileRes = IoUtil.readUtf8(fileResource.getInputStream());
+        System.out.println(fileRes);
+    }
+
+    @Test
     public void test_BeanFactory() {
         // 1. 初始化 BeanFactory
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 
-        // 2. 注入依赖 BeanDefinition userMapper
-        beanFactory.registerBeanDefinition("userMapper", new BeanDefinition(UserMapper.class));
+        // 2. 解析xml
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+        beanDefinitionReader.loadBeanDefinitions("classpath:spring.xml");
 
-        // 3. 注入 userService (设置属性id和userMapper)
-        PropertyValues propertyValues = new PropertyValues();
-        propertyValues.addPropertyValue(new PropertyValue("id", "10001"));
-        // 属性为 Bean 时，注入的是一个 BeanReference，以便于执行相关注入属性操作进行依赖 Bean的逻辑
-        propertyValues.addPropertyValue(new PropertyValue("userMapper", new BeanReference("userMapper")));
-        beanFactory.registerBeanDefinition("userService", new BeanDefinition(UserService.class, propertyValues));
-
-        // 4. 获取 Bean
+        // 3. 获取bean
         UserService userService = (UserService) beanFactory.getBean("userService");
         userService.getUserInfo();
     }
