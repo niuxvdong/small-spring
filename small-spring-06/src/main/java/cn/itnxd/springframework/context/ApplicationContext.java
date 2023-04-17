ca99f1b0a42ac78692f812d7f5d60e05f759b662
niuxvdong <158903258@qq.com>
2023-04-17 23:52:36 +0800

    feat：第六章-添加ApplicationContext定义

---
+++ small-spring-06/src/main/java/cn/itnxd/springframework/context/ApplicationContext.java
@@ -0,0 +1,18 @@
+package cn.itnxd.springframework.context;
+
+import cn.itnxd.springframework.beans.factory.ListableBeanFactory;
+
+/**
+ * @Author niuxudong
+ * @Date 2023/4/17 22:57
+ * @Version 1.0
+ * @Description ApplicationContext除了拥有BeanFactory的所有功能外，还支持特殊类型bean，如：BeanFactoryPostProcessor
+ *              和BeanPostProcessor的自动识别、资源加载、容器事件和监听器、国际化支持、单例bean自动初始化等。
+ *              BeanFactory是spring的基础设施，面向spring本身；而ApplicationContext面向spring的使用者，
+ *
+ *              核心流程逻辑在：AbstractApplicationContext#refresh方法。
+ *
+ *              本接口为顶层核心接口，暂不定义方法，具有父接口的获取bean的方法，包括根据类型获取
+ */
+public interface ApplicationContext extends ListableBeanFactory {
+}
