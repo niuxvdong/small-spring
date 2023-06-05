fb7db1f743f8995f61f4f5f2781e1c7ce8dd7a2d
niuxvdong <158903258@qq.com>
2023-06-05 23:25:10 +0800

    feat：手动向容器中添加支持处理三大注解的解析器 Resolver

--- small-spring-13/src/main/java/cn/itnxd/springframework/beans/factory/PropertyPlaceholderConfigurer.java
+++ small-spring-13/src/main/java/cn/itnxd/springframework/beans/factory/PropertyPlaceholderConfigurer.java
@@ -7,6 +7,7 @@ import cn.itnxd.springframework.beans.factory.config.BeanDefinition;
 import cn.itnxd.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import cn.itnxd.springframework.core.io.DefaultResourceLoader;
 import cn.itnxd.springframework.core.io.Resource;
+import cn.itnxd.springframework.utils.StringValueResolver;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -49,25 +50,18 @@ public class PropertyPlaceholderConfigurer implements BeanFactoryPostProcessor {
                 Object value = propertyValue.getValue();
                 // 只能解析 String 类型的属性值
                 if (value instanceof String) {
-                    String strVal = (String) value;
-                    StringBuffer buf = new StringBuffer(strVal);
-                    int startIndex = strVal.indexOf(PLACEHOLDER_PREFIX);
-                    int endIndex = strVal.indexOf(PLACEHOLDER_SUFFIX);
-                    // ${} 合法
-                    if (startIndex != -1 && endIndex != -1 && startIndex < endIndex) {
-                        // key 去掉 ${} 得到属性 key
-                        String propKey = strVal.substring(startIndex + 2, endIndex);
-                        // 从properties配置文件中得到对应的 属性 value
-                        String propVal = properties.getProperty(propKey);
-                        // 将 ${xxx} 替换为 对应的配置文件中的值
-                        buf.replace(startIndex, endIndex + 1, propVal);
-                        // 将属性k v 对添加到 propertyValues 里
-                        // 这里动态向 propertyValues 添加元素，会造成死循环吗？不会，会在 index 的判断下跳出循环。
-                        propertyValues.addPropertyValue(new PropertyValue(propertyValue.getName(), buf.toString()));
-                    }
+                    // 解析占位符从配置中拿到对应值返回
+                    value = resolvePlaceholder((String) value, properties);
+                    // 将属性k v 对添加到 propertyValues 里
+                    // 这里动态向 propertyValues 添加元素，会造成死循环吗？不会，会在 index 的判断下跳出循环。
+                    propertyValues.addPropertyValue(new PropertyValue(propertyValue.getName(), value));
                 }
             }
         }
+
+        // 增加：往容器中添加字符解析器，供解析 @Value 注解使用
+        StringValueResolver valueResolver = new PlaceholderResolvingStringValueResolver(properties);
+        beanFactory.addEmbeddedValueResolver(valueResolver);
     }
 
     private Properties loadProperties() {
@@ -84,11 +78,56 @@ public class PropertyPlaceholderConfigurer implements BeanFactoryPostProcessor {
         }
     }
 
+    /**
+     * 解析占位符拿到真实值返回
+     * @param strVal
+     * @param properties
+     * @return
+     */
+    private String resolvePlaceholder(String strVal, Properties properties) {
+        StringBuffer buf = new StringBuffer(strVal);
+        int startIndex = strVal.indexOf(PLACEHOLDER_PREFIX);
+        int endIndex = strVal.indexOf(PLACEHOLDER_SUFFIX);
+        // ${} 合法
+        if (startIndex != -1 && endIndex != -1 && startIndex < endIndex) {
+            // key 去掉 ${} 得到属性 key
+            String propKey = strVal.substring(startIndex + 2, endIndex);
+            // 从properties配置文件中得到对应的 属性 value
+            String propVal = properties.getProperty(propKey);
+            // 将 ${xxx} 替换为 对应的配置文件中的值
+            buf.replace(startIndex, endIndex + 1, propVal);
+        }
+        return buf.toString();
+    }
+
     public String getLocation() {
         return location;
     }
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    /**
+     * 增加占位符解析器，添加到容器中用来解析 @Value 注解
+     */
+    private class PlaceholderResolvingStringValueResolver implements StringValueResolver {
+
+        // 持有 properties 配置文件
+        private final Properties properties;
+
+        public PlaceholderResolvingStringValueResolver(Properties properties) {
+            this.properties = properties;
+        }
+
+        /**
+         * 将传入的 ${} 进行解析拿到真实值返回
+         * @param strVal
+         * @return
+         * @throws BeansException
+         */
+        public String resolveStringValue(String strVal) throws BeansException {
+            return resolvePlaceholder(strVal, properties);
+        }
     }
 }
