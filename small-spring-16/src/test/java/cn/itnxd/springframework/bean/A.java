7dfd36c6f5e99b04e2699fbdcc9e25267a1d4616
niuxvdong <158903258@qq.com>
2023-06-10 16:46:03 +0800

    feat：添加循环依赖测试

---
+++ small-spring-16/src/test/java/cn/itnxd/springframework/bean/A.java
@@ -0,0 +1,20 @@
+package cn.itnxd.springframework.bean;
+
+/**
+ * @Author niuxudong
+ * @Date 2023/6/10 16:37
+ * @Version 1.0
+ * @Description
+ */
+public class A {
+
+    private B b;
+
+    public B getB() {
+        return b;
+    }
+
+    public void setB(B b) {
+        this.b = b;
+    }
+}
---
+++ small-spring-16/src/test/java/cn/itnxd/springframework/bean/B.java
@@ -0,0 +1,20 @@
+package cn.itnxd.springframework.bean;
+
+/**
+ * @Author niuxudong
+ * @Date 2023/6/10 16:37
+ * @Version 1.0
+ * @Description
+ */
+public class B {
+
+    private A a;
+
+    public A getA() {
+        return a;
+    }
+
+    public void setA(A a) {
+        this.a = a;
+    }
+}
